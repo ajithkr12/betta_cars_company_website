c1a1f1f5e3b28864cea29e62a3f0d89d4c8bcd9d
ajith <ajithkr095@gmail.com>
2024-08-26 21:47:55 +0530

    some new pages

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-const swiper = new Swiper(".slider-wrapper", {
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 36,
+// const swiper = new Swiper(".slider-wrapper", {
+//   loop: true,
+//   grabCursor: true,
+//   spaceBetween: 36,
 
-  // Pagination bullets
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
+//   // Pagination bullets
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//     dynamicBullets: true,
+//   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
 
-  // Responsive breakpoints
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  },
-});
+//   // Responsive breakpoints
+//   breakpoints: {
+//     0: {
+//       slidesPerView: 1,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//     },
+//     1024: {
+//       slidesPerView: 3,
+//     },
+//   },
+// });
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+  $(".owl-carousel-common").owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
@@ -50,6 +50,29 @@ $(document).ready(function () {
     },
   });
 });
+
+$(document).ready(function () {
+  $(".owl-carousel-testimonial").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1200: {
+        items: 2,
+      },
+      1800: {
+        items: 2,
+      },
+    },
+  });
+});
+
 // document.addEventListener("DOMContentLoaded", function () {
 //   const navbar = document.getElementById("navbar");
 //   const menu = document.getElementById("main-menu");
